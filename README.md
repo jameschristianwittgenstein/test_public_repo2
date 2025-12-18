9070fd7dfcd3f4461ae6dd141a3cea50ce340140
James <tan.james@solize.com>
2025-12-18 13:18:34 +0900

    update sample code from aws.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Test Repository
+
+This is a test repository for AWS Lambda GitHub integration.
+
+## Features
+- File reading
+- Pull request management
+- Code search
---
+++ src/main.py
@@ -0,0 +1,7 @@
+def hello_world():
+    """A simple test function"""
+    print("Hello from GitHub!")
+    return "Success"
+
+if __name__ == "__main__":
+    hello_world()
